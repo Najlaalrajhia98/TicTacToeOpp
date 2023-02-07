49c34440de25dd3582d47e5ef4926462f3feaccb
Najlaalrajhia98 <najla.alrajhi@tra.gov.om>
2023-02-07 22:00:37 +0400

    updated

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         StateManager stateManager = new StateManager();
         System.out.println("Do you want to resume a game (yes/no)?");
-        String choice = sc.nextLine();
+        String choice = userInput.nextLine();
 
         Symbol[] symbols = new Symbol[]{new Symbol("X"), new Symbol("O")};
         Board board = new Board(symbols);
@@ -18,15 +18,15 @@ public class App {
         if (choice.equalsIgnoreCase("yes")){
             stateManager.loadGame(board,players,turn);
             board.printBoard(board.getSlots());
-            playGame(board, sc, stateManager, symbols, players, turn);
+            playGame(board, userInput, stateManager, symbols, players, turn);
         }
         else if  (choice.equalsIgnoreCase("no")) {
 
             System.out.println(" Player 1 Enter your symbol (X or O):");
-            String playerSymbol = sc.nextLine();
+            String playerSymbol = userInput.nextLine();
             while (!playerSymbol.equalsIgnoreCase("X") && !playerSymbol.equalsIgnoreCase("O")) {
                 System.out.println("Invalid symbol. Enter X or O:");
-                playerSymbol = sc.nextLine();
+                playerSymbol = userInput.nextLine();
             }
 
             if (playerSymbol.equalsIgnoreCase("X")) {
@@ -36,11 +36,11 @@ public class App {
                 players[0] = new Player("Player 2", symbols[0]);
                 players[1] = new Player("Player 1", symbols[1]);
             }
-            playGame(board, sc, stateManager, symbols, players, turn);
+            playGame(board, userInput, stateManager, symbols, players, turn);
         }
         }
 
-    private static void playGame (Board board,Scanner sc,StateManager stateManager,Symbol[] symbols,Player[] players,int turn){
+    private static void playGame (Board board,Scanner userInput,StateManager stateManager,Symbol[] symbols,Player[] players,int turn){
 
         while (!board.isFull() && !board.isWinner(players[turn % 2].getSymbol())) {
             System.out.println("Current board:");
@@ -51,8 +51,8 @@ public class App {
             int col = 0;
             do {
                 System.out.println(players[turn % 2].getName() + " turn. Enter row and column (0-based):");
-                row = sc.nextInt();
-                col = sc.nextInt();
+                row = userInput.nextInt();
+                col = userInput.nextInt();
                 if (row < 3 && col < 3) {
                     index = true;
                 } else {
@@ -63,8 +63,8 @@ public class App {
             while (!board.setSlot(row, col, players[turn % 2].getSymbol())) {
                 System.out.println("This slot is already taken. Enter another row and column:");
                 do {
-                    row = sc.nextInt();
-                    col = sc.nextInt();
+                    row = userInput.nextInt();
+                    col = userInput.nextInt();
                     if (row < 3 && col < 3) {
                         index = true;
                     } else {
@@ -80,13 +80,13 @@ public class App {
             stateManager.saveGame(board,players,turn);
         }
 
-        System.out.println("Current board:");
-        board.printBoard(board.getSlots());
-        if (board.isFull() && !board.isWinner(players[0].getSymbol()) && !board.isWinner(players[1].getSymbol())) {
+             System.out.println("Current board:");
+            board.printBoard(board.getSlots());
+            if (board.isFull() && !board.isWinner(players[0].getSymbol()) && !board.isWinner(players[1].getSymbol())) {
             System.out.println("It's a draw!");
-        } else if (board.isWinner(players[0].getSymbol())) {
+            } else if (board.isWinner(players[0].getSymbol())) {
             System.out.println(players[0].getName() + " wins!");
-        } else if (board.isWinner(players[1].getSymbol())) {
+            } else if (board.isWinner(players[1].getSymbol())) {
             System.out.println(players[1].getName() + " wins!");
         }
     }
